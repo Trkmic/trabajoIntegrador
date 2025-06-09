3b0d9aba60084a50c6d1b6f0a9e40311f222b0fe
Ignacio Trkmic Torres <157857073+Trkmic@users.noreply.github.com>
2025-06-08 23:14:48 -0300

    Add files via upload

--- trabajoIntegrador/js/bienvenida.js
+++ trabajoIntegrador/js/bienvenida.js
@@ -4,8 +4,8 @@ const boton = document.querySelector("button");
         const nombre = document.getElementById("nombre").value.trim();
 
         if (nombre !== "") {
-            window.location.href = "../html/productos.html";
-        } else {
+            window.location.href = "../html/main.html";
+        } else{
             alert("Por favor, ingrese su nombre.");
         }
-    });
+});
